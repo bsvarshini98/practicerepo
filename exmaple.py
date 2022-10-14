7c949179901ff63090d9335b453f948956b39e20
bsvarshini98 <115773653+bsvarshini98@users.noreply.github.com>
2022-10-14 12:49:15 +0530

    Create exmaple.py

---
+++ exmaple.py
@@ -0,0 +1,19 @@
+def CheckOddwords(strlist):
+    x = len([i for i in strlist])
+    output = 0
+    for eachdata in strlist:
+        j = 0
+        for i in eachdata:
+            j = j + 1
+        if (j % 2 == 1):
+            output = output + 1
+    if len(strlist):
+        return output
+    else:
+        return -1
+l1 = []
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    element = str(input("enter elemenst {} \n".format(i)))
+    l1.append(element)
+print("number of odd lenght words in string are =",CheckOddwords(l1))
